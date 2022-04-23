34eb0916c40d0b35640ddc91a5b7e75d164c6a89
Raka Gunarto <rakagunarto@gmail.com>
2022-04-24 02:02:44 +0800

    library interface

--- user/user.h
+++ user/user.h
@@ -41,3 +41,21 @@ void free(void*);
 int atoi(const char*);
 int memcmp(const void *, const void *, uint);
 void *memcpy(void *, const void *, uint);
+
+// uwindow.c
+typedef struct window *window_handle; // opaque type
+struct window_dim
+{
+    uint16 width;
+    uint16 height;
+};
+struct windowevent;
+
+window_handle window_create();
+struct window_dim window_getdimensions(window_handle win);
+struct windowevent window_pollevent(window_handle win, int max_evts);
+void window_drawsprite(window_handle win, uint x, uint y, uint w, uint h, uint8 *data);
+void window_drawchar(window_handle win, uint x, uint y, char c);
+void window_drawline(window_handle win, uint x1, uint y1, uint x2, uint y2);
+void window_drawrect(window_handle win, uint x, uint y, uint w, uint h, uint8 color);
+void window_drawcircle(window_handle win, uint x, uint y, uint r);
